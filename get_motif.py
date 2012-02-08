011ae7e973fd2b8eabf8d7fa8626fe042aee1654
Zach Stednick <zstednic@fhcrc.org>
2012-02-07 16:41:30 -0800

    now with argparse

--- get_motif.py
+++ get_motif.py
@@ -4,10 +4,10 @@ from collections import defaultdict
 from operator import itemgetter
 import argparse
 
-
-test, motifs = [], []
+test, motifs= [], []
 def motif_finder(motif, count, n):
     s = 0
+    n = int(n)
     for x in range(len(motif)):
         k = motif[s:s+n]
         if len(k) == n:
@@ -24,5 +24,22 @@ def main():
     parser.add_argument('length', help = "Motif length")
     parser.add_argument('outfile', help = "Outfile to write summary stats to")
     args = parser.parse_args()
+    
+    #test, motifs = [], []
+    with open(args.infile, 'r') as infile:
+        lines = infile.readlines()
+        for x in lines:
+            vals = x.split(" ")
+            motif_finder(vals[0].rstrip("\n"), int(vals[1].rstrip("\n")),
+                    args.length)
+        d = defaultdict(int)
+        for x in test:
+            d[x[0]] += int(x[1])
+
+    print sorted(d.iteritems(), key = itemgetter(1), reverse = True)
+
+
+if __name__ == "__main__":
+    main()
 
 
